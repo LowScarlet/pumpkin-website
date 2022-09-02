be3a586728d012ed7ea25d7b7aa8713b09972330
Low_Scarlet <namaetohyaakiradesu@gmail.com>
2022-09-02 10:26:45 +0700

    !

--- frontend/src/components/layout/index.tsx
+++ frontend/src/components/layout/index.tsx
@@ -18,7 +18,7 @@ interface LayoutProps {
 
 export default function Layout(props: LayoutProps) {
     // Get public data from backend
-    let { data } = useSWR(`${API_URL()}/api`, fetcher)
+    let { data } = useSWR(`${API_URL()}/api/`, fetcher)
     console.log(API_URL())
     
     // Theme
