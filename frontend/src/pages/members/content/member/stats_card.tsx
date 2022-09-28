d1c096dc86895cc1aa5ef363f3f1676b4b6ce57f
Low_Scarlet <namaetohyaakiradesu@gmail.com>
2022-09-28 22:24:27 +0700

    fixed

--- frontend/src/pages/members/content/member/stats_card.tsx
+++ frontend/src/pages/members/content/member/stats_card.tsx
@@ -38,19 +38,19 @@ export default function Main(props: any) {
     <div className="shadow card">
       <div className="card-body text-center text-dark">
         <div className="row">
-          <div className="col-4">
+          <div className="col-4 px-sm-2 px-0">
             <div className="border-bottom border-5 border-success">
               <h5 className="text-muted mt-1 mb-2 fw-normal"><i className="pe-2 bi bi-eye-fill"></i>Views</h5>
               <h2 className="mb-0 fw-bold">{millify(props.data.profile.views)}</h2>
             </div>
           </div>
-          <div className="col-4">
+          <div className="col-4 px-sm-2 px-0">
             <div className="border-bottom border-5 border-primary">
               <h5 className="text-muted mt-1 mb-2 fw-normal"><i className="pe-2 bi bi-trophy-fill"></i>Crown</h5>
               <h2 className="mb-0 fw-bold">{millify(0)}</h2>
             </div>
           </div>
-          <div className="col-4">
+          <div className="col-4 px-sm-2 px-0">
             <div className="border-bottom border-5 border-warning">
               <h5 className="text-muted mt-1 mb-2 fw-normal"><i className="pe-2 bi bi-gift-fill"></i>Gift</h5>
               <h2 className="mb-0 fw-bold">{millify(0)}</h2>
